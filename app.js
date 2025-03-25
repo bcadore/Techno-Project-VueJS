9af22ef0405fd5d76daffaf94e3a91ba5736b2de
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-03-25 20:45:18 -0300

    Add funcionalidade para fechar o Modal.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const vm = new Vue({
     el: "#app",
     data: {
         produtos: [],
-        produto: false
+        produto: false,
     },
     filters: {
         numeroPreco(valor) {
@@ -23,6 +23,16 @@ const vm = new Vue({
                 .then(r => {
                     this.produto = r;
                 })
+        },
+        abrirModal(id) {
+            this.fetchProduto(id);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        },
+        fecharModal({ target, currentTarget }) {
+            if (target === currentTarget) this.produto = false;
         }
     },
     created() {
