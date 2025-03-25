d9bf0a846c14e6d17a7a23157b7516435b91e6bb
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-03-24 23:27:59 -0300

    Adicionado fetch dos dados de cada produto.

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        produtos: []
+        produtos: [],
+        produto: false
     },
     filters: {
         numeroPreco(valor) {
-            return valor.toLocaleString("pt-BR", {style:"currency", currency:"BRL"});
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
         }
     },
     methods: {
         fetchProdutos() {
             fetch("./api/produtos.json")
                 .then(r => r.json())
-                .then(r => { 
-                    this.produtos = r; 
+                .then(r => {
+                    this.produtos = r;
+                })
+        },
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(r => {
+                    this.produto = r;
                 })
         }
     },
